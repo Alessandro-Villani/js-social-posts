8ab546f1451132ab3d5f3b66459f5ae02bb604ae
Alessandro-Villani <alessvill@libero.it>
2022-12-23 16:28:10 +0100

    tweaked date conversion function

--- js/script.js
+++ js/script.js
@@ -59,9 +59,11 @@ console.log(targetPostList);
 
 //Function to convert date
 const convertDate = (date) => {
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
     const year = date.getFullYear();
+    if (day < 10) day = '0' + day;
+    if (month < 10) month = '0' + month;
     return `${day}/${month}/${year}`
 }
 
